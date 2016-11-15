41d98ec8faf598f51935493817508fa651d85f64
Paweł Fluder <pawelfluder@gmail.com>
2016-11-15 15:40:34 +0100

    funkcja trygonometryczna cos

--- trygonometria.py
+++ trygonometria.py
@@ -11,4 +11,20 @@ def sin(x):
         return 0
     else: 
         return False
+
+def cos(x):
+    if x==90:
+        return 0
+    elif x==60:
+        return 0.5
+    elif x==45:
+        return (2 ** (0.5))/2
+    elif x==30:
+        return (3 ** (0.5))/2
+    elif x==0:
+        return 1
+    else:
+        return False
+
 print(sin(45))
+print(cos(45))
